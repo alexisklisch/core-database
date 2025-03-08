74f4c92d79e5462548e58429c6c286a53763dd5c
Alexis Fleitas Klisch <unbosquense@gmail.com>
2025-03-08 07:21:13 -0300

    fix: make priceType field in operationPrices schema not nullable

--- src/db/schemas/operationPrices.schema.ts
+++ src/db/schemas/operationPrices.schema.ts
@@ -6,7 +6,7 @@ export const operationPricesSchema = sqliteTable("operation_prices", {
   operationPriceId: int('operation_price_id').primaryKey({ autoIncrement: true }),
   amount: int('operation_price_amount').notNull(),
   currencyId: text('operation_price_currency_id', { length: 3 }).notNull(),
-  priceType: text('operation_price_type', { enum: ['listing', 'maintenance', 'old'] })
+  priceType: text('operation_price_type', { enum: ['listing', 'maintenance', 'old'] }).notNull()
 })
 
 export const operationPricesRelations = relations(operationPricesSchema, ({ one }) => ({
